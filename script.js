eb68dd85055a77bf50764f2d4b2b18c86e5c0560
Naveen <naveensharma07005@gmail.com>
2024-11-01 15:03:08 +0530

    completed till page 4

--- script.js
+++ script.js
@@ -7,10 +7,9 @@ function loader() {
         delay: 0.5,
         duration: 0.6,
         stagger: 0.3
-    })
+    });
 
-    // timer and NOW text
-    tl.from("#line1-part1 , .line h3 ", {
+    tl.from("#line1-part1 , .line h3", {
         opacity: 0,
         duration: 0.3,
         stagger: 0.2,
@@ -21,18 +20,18 @@ function loader() {
             setInterval(function () {
                 if (grow < 100) {
                     h5Timer.innerHTML = grow++;
+                } else {
+                    h5Timer.innerHTML = grow;
+                    clearInterval(setInterval);
                 }
-                else {
-                    h5Timer.innerHTML = grow
-                    clearInterval(setInterval)
-                }
-            }, 35)
+            }, 35);
         }
-    })
-    tl.to(".line h3 ", {
+    });
+
+    tl.to(".line h3", {
         opacity: 1,
         animationName: "anime"
-    })
+    });
 
     tl.to(".loader", {
         opacity: 0,
@@ -40,71 +39,47 @@ function loader() {
         delay: 3.5,
         onComplete: function () {
             document.querySelector(".loader").style.display = "none";
-<<<<<<< HEAD
-            document.querySelector(".page1").style.display = "block"; // Show Page 1
-=======
-<<<<<<< HEAD
-            document.querySelector(".page1").style.display = "block"; // Show Page 1
-=======
-            document.querySelector(".page1").style.display = "block"; 
->>>>>>> f7de177 (completed till Page 4)
->>>>>>> 70ab65f (p)
+            document.querySelector(".page1").style.display = "block";
             page1();
         }
-    })
+    });
 
     tl.from(".page1", {
         y: 1000,
         opacity: 0,
         ease: "power2.out",
         duration: 0.2,
-    })
+    });
     tl.from("#navbar", {
         opacity: 0,
         y: 100,
         duration: 2,
         delay: 0.5
-    })
+    });
 }
-<<<<<<< HEAD
+
 loader();
-// document.querySelector(".loader").style.display = "none";
-=======
-<<<<<<< HEAD
-loader();
-// document.querySelector(".loader").style.display = "none";
-=======
-// loader();
-document.querySelector(".loader").style.display = "none";
->>>>>>> f7de177 (completed till Page 4)
->>>>>>> 70ab65f (p)
 
 function page1() {
-
-    tl = gsap.timeline();
-
+    var tl = gsap.timeline();
     tl.from(".hero-line h2", {
         y: 1500,
         opacity: 0,
         duration: 2,
         stagger: 0.2
-    })
-
+    });
 }
+
 page1();
 
 function cursor() {
-
     document.addEventListener("mousemove", function (data) {
-        // console.log(data.clientX)
         gsap.to("#cursor", {
             left: data.x,
             top: data.y,
-        }
-        )
-    })
+        });
+    });
     Shery.makeMagnet(".nav-part2 h4");
 }
+
 cursor();
-
-
